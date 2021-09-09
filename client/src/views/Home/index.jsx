191fff7a356801f7d03b44ae585d72ebd0cb45c9
tholeb <thomasleb.49@gmail.com>
2021-09-09 21:53:44 +0200

    + Added pages routes

---
+++ client/src/views/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Forecast from '../../components/Forecast';
+
+const index = () => {
+	return (
+		<div>
+			Home Page
+
+			<Forecast/>
+		</div>
+	);
+};
+
+export default index;
