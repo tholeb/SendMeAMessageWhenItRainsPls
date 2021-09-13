ab1ef815fb07f68528bd4b2d110f2ccc94b2f808
tholeb <thomasleb.49@gmail.com>
2021-09-13 21:04:53 +0200

    feat(MobileNav): classes

--- client/src/components/nav/AppMobileNav.jsx
+++ client/src/components/nav/AppMobileNav.jsx
@@ -18,10 +18,10 @@ const AppMobileNav = () => {
 			leaveFrom="opacity-100 scale-100"
 			leaveTo="opacity-0 scale-95"
 		>
-			<Popover.Panel focus className="absolute inset-x-0 top-0 p-2 transition transform origin-top-right md:hidden">
-				<div className="bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50">
+			<Popover.Panel focus className="md:hidden absolute inset-x-0 top-0 p-2 transition transform origin-top-right">
+				<div className="bg-white rounded-lg divide-y-2 divide-gray-50 ring-1 ring-black ring-opacity-5 shadow-lg">
 					<div className="px-5 pt-5 pb-6">
-						<div className="flex items-center justify-between">
+						<div className="flex justify-between items-center">
 							<div>
 								<img
 									className="w-auto h-8"
@@ -30,7 +30,7 @@ const AppMobileNav = () => {
 								/>
 							</div>
 							<div className="-mr-2">
-								<Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Popover.Button className="inline-flex justify-center items-center p-2 text-gray-400 hover:text-gray-500 bg-white hover:bg-gray-100 rounded-md focus:ring-2 focus:ring-inset focus:ring-indigo-500 focus:outline-none">
 									<span className="sr-only">Close menu</span>
 									<XIcon className="w-6 h-6" aria-hidden="true" />
 								</Popover.Button>
@@ -42,7 +42,7 @@ const AppMobileNav = () => {
 									<a
 										key={item.name}
 										href={item.href}
-										className="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50"
+										className="flex items-center p-3 -m-3 hover:bg-gray-50 rounded-md"
 									>
 										<item.icon className="flex-shrink-0 w-6 h-6 text-indigo-600" aria-hidden="true" />
 										<span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
@@ -51,7 +51,7 @@ const AppMobileNav = () => {
 							</nav>
 						</div>
 					</div>
-					<div className="px-5 py-6 space-y-6">
+					<div className="py-6 px-5 space-y-6">
 						<div>
 							<p className="mt-6 text-base font-medium text-center text-gray-500">
                   Made by{' '}
