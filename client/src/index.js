194dd73c480c9807e2b9e8cea35ebfa958200d5a
tholeb <thomasleb.49@gmail.com>
2021-09-13 21:00:40 +0200

    feat(base): tailwind style output + react base stuff

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
+import './assets/styles/output.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
